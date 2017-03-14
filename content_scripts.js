ec604ff28aa4c65a998d4c52c342c1b8c765a302
2016GitBuild <wld74769@163.com>
2017-03-14 10:45:50 +0800

    添加按钮样式，及出现动画

--- content_scripts.js
+++ content_scripts.js
@@ -1,8 +1,10 @@
 var w = $("#body_login");
 var accouts = [];
-accouts.push(["东东", "cdwudong@300.cn", "cd123456"]);
+accouts.push(["吴东", "cdwudong@300.cn", "cd123456"]);
+accouts.push(["彭雄", "cdpengxiong@300.cn", "asdf123"]);
+accouts.push(["陈秋霖", "cdchenqiulin@300.cn", "cd123456"]);
 $.each(accouts, function(i, v) {
-	w.append("<div _i='"+i+"' class='w_buttons accouts_" + (i+1) + "'>"+accouts[i][0]+"</div>");
+	w.append("<div _i='"+i+"' class='w_buttons button button-3d button-primary button-rounded accouts_" + (i+1) + "'>"+accouts[i][0]+"</div>");
 });
 
 var loginNameId = $("#loginNameId");
@@ -12,10 +14,16 @@ var loginInBtn = $("#loginInBtn");
 loginNameId.val("");
 passwordId.val("");
 
-$(".w_buttons").click(function() {
+var w_buttons = $(".w_buttons").click(function() {
 	var i = $(this).attr("_i");
 	var t = accouts[i];
 	loginNameId.val(t[1]);
 	passwordId.val(t[2]);
 	loginInBtn.trigger("click");
+});
+
+$(window).load(function() {
+	w_buttons.each(function(i) {
+		$(this).delay(i*300).animate({"left":0}, 300);
+	});
 });
